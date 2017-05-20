8f91b0bc150ca906dad11a6f32a57ff4817fb6a7
Florian Uhde <floar@gmx.de>
2017-05-20 09:49:48 +0200

    Added test for dapp-url and bot-url
    both url get included if they exist in pakcage.json

--- cli.js
+++ cli.js
@@ -73,7 +73,10 @@ function getCurrentPackageData() {
         var json = JSON.parse(fs.readFileSync(process.cwd() + '/package.json', 'utf8'));
         obj["name"] = json.name;
         obj["whisper-identity"] = "dapp-" + fromAscii(json.name);
-        obj["dapp-url"] = "http://localhost:" + (cli.dappPort || defaultDAppPort);
+		if(json["dapp-url"])
+			obj["dapp-url"] = json["dapp-url"];
+		if(json["bot-url"])
+			obj["bot-url"] = json["bot-url"];
     }
     return obj;
 }
